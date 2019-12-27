a33cdf4e10b772f9711977572c30a459fc2061e8
Christian Seifert <christian.seifert@betterdoc.de>
2019-12-27 12:27:47 +0100

    Adjust to new engine

--- lib/betterdoc/website/templating/controllers/concerns/templating_concern.rb
+++ lib/betterdoc/website/templating/controllers/concerns/templating_concern.rb
@@ -12,14 +12,18 @@ module Betterdoc
               view_content = render_to_string(view, view_options)
 
               template_engine = create_website_template_engine
-              template_engine.content = view_content
-              template_content = template_engine.resolve_content
+              template_context = create_website_template_context(view_content)
+              template_content = template_engine.resolve_content(template_context)
 
               render options.merge(body: template_content, layout: false, content_type: 'text/html')
             end
 
             def create_website_template_engine
               Betterdoc::Website::Templating::Engine.new
+            end
+
+            def create_website_template_context(content)
+              { content: content }
             end
 
           end
